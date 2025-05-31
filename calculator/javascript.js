b04e8f78671c717c3d873096c68273c402ba0788
elifnull <Elifnull@protonmail.com>
2025-05-30 21:53:20 -0400

    added functions: +,-,/,x

---
+++ calculator/javascript.js
@@ -0,0 +1,26 @@
+/*
+Functions that will be called
+*/
+ function sumTwoNumbers (num1, num2){
+    return num1 + num2;
+ };
+
+ function multiplyTwoNumbers (num1, num2){
+    return num1*num2;
+ };
+
+ function subtractTwoNumbers (num1, num2){
+    return num1 - num2;
+ };
+
+ function divideTwoNumbers (num1, num2){
+    return num1/num2;
+ };
+
+ /*
+Logic that organizes functions
+*/
+
+function operationsCaller (num1,operator,num2){
+
+};
