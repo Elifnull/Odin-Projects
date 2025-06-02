afcf26bbd281e5c6bcb133366cdc81d8ce4107d4
elifnull <elifnull@protonmail.com>
2025-06-02 12:17:41 -0400

    operatorl logic updated, operators abstracted

--- calculator/javascript.js
+++ calculator/javascript.js
@@ -24,12 +24,26 @@ dynamic Variables
  const operatorValue = [];
  const secondNumber = 0;
 
-
- /*
+const [additionOperator, subtractionOperator, multiplicationOperator, divisionOperator] = ["+","-","x","/"];
+Operator
+/*
 Logic that organizes functions
 */
 
- function operatorLogic (num1,operator,numb2){
-
+ function operatorLogic (num1,operator,num2){
+   switch (operator){
+      case additionOperator:
+         sumTwoNumbers(num1,num2);
+         break;
+      case subtractionOperator:
+         subtractTwoNumbers(num1,num2);
+         break;
+      case multiplicationOperator:
+         multiplyTwoNumbers(num1,num2);
+         break;
+      case divisionOperator:
+         divideTwoNumbers(num1,num2);
+         break;
+   }
  };
 
